bbc63dcfecee530cc44f69cd4ecad5878db17646
George Gardner <Gpgardner@yahoo.com>
2021-04-15 18:20:42 -0400

    added isFull, method to setFull may use later

--- Percolation/Node.js
+++ Percolation/Node.js
@@ -1,8 +1,22 @@
 module.exports = class Node {
+  row;
+  col;
+  isOpen;
+  isFull;
+
   constructor(row, col, isOpen) {
     this.row = row;
     this.col = col;
     this.isOpen = isOpen;
+    this.isFull = false;
+  }
+
+  setFull() {
+    if (this.isOpen) {
+      this.isFull = true;
+    } else {
+      throw new Error(`This node is blocked`);
+    }
   }
 
   _print() {
