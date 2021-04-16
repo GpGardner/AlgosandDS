00052958e28716a864e31f78b25a7665ea19e230
George Gardner <Gpgardner@yahoo.com>
2021-04-15 22:53:45 -0400

    test class update

--- Percolation/Test.js
+++ Percolation/Test.js
@@ -1,4 +1,5 @@
 const Percolation = require('./Percolation');
+const colors = require('colors/safe');
 
 // const perc = new Percolation(4, 0);
 // console.log(perc.isOpenFast(0, 1));
@@ -12,13 +13,21 @@ const Percolation = require('./Percolation');
 // perc._print();
 // console.log(` Does this percolate? : ${perc.isFull(3, 3)}`);
 
-const perc = new Percolation(3, 0);
-console.log(perc.isOpenFast(0, 1));
-console.log(perc.numberOfOpenSites());
-perc._print();
-perc.open(0, 1);
+const perc = new Percolation(4, 0);
+// console.log(perc._isOpen(0, 1));
+// console.log(perc.numberOfOpenSites());
+// perc._print();
+perc.open(0, 0);
+perc.open(1, 0);
 perc.open(1, 1);
-perc.open(2, 1);
-perc._findNode(0, 1)._print();
+// perc.open(1, 2);
+perc.open(1, 3);
+
 perc._print();
-console.log(` Is this full? : ${perc.isFull(2, 0)}`);
+
+perc.grid.nodes.forEach((node) => {
+  let string = node._print();
+  perc.isFull(node.row, node.col)
+    ? console.log(colors.green(string))
+    : console.log(colors.red(string));
+});
