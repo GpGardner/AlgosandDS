3ba0432625418216b9153ee221cfb03d7d848597
George Gardner <Gpgardner@yahoo.com>
2021-04-17 16:17:26 -0400

    More readable test

--- Percolation/Test.js
+++ Percolation/Test.js
@@ -1,38 +1,42 @@
 const Percolation = require('./Percolation');
 const colors = require('colors/safe');
 
-// const perc = new Percolation(4, 0);
-// console.log(perc.isOpenFast(0, 1));
-// console.log(perc.numberOfOpenSites());
-// perc._print();
-// perc.open(0, 1);
-// perc.open(1, 1);
-// perc.open(2, 1);
-// perc.open(3, 1);
-// perc._findNode(0, 1)._print();
-// perc._print();
-// console.log(` Does this percolate? : ${perc.isFull(3, 3)}`);
+const perc = new Percolation(9);
+let width = perc.gridSize - 1;
 
-const perc = new Percolation(4, 0);
 // console.log(perc._isOpen(0, 1));
 // console.log(perc.numberOfOpenSites());
 // perc._print();
-perc.open(0, 0);
-perc.open(1, 0);
-perc.open(2, 0);
-perc.open(3, 0);
-
-perc.open(3, 1);
-
+// console.log(perc._isOpen(1, 2));
 // perc.open(1, 2);
-// perc.open(3, 0);
+// perc.open(0, 2);
+// perc._print();
+// console.log(perc.ifFull2(1, 2));
 
-perc._print();
-console.log(perc.percolates());
+function randNum() {
+  return Math.round(Math.random() * width);
+}
 
-perc.grid.nodes.forEach((node) => {
-  let string = node._print();
-  perc.isFull(node.row, node.col)
-    ? console.log(colors.green(string))
-    : console.log(colors.red(string));
-});
+// perc.open(0, 2);
+// perc.open(1, 1);
+// perc.open(2, 2);
+// perc.open(3, 2);
+// console.log(perc._isOpen(3, 2));
+// perc._print();
+// console.log(`log ${perc.isFull(3, 2)}`);
+
+// console.log(`TEST: ${perc.percolates()}`);
+
+setInterval(() => {
+  if (perc.percolates()) {
+    return;
+  }
+  let value1 = randNum();
+  let value2 = randNum();
+
+  console.log(`Row: ${value1}, Column ${value2}`);
+  if (perc._findNode(value1, value2).open === false) {
+    perc.open(value1, value2);
+    perc._print();
+  }
+}, 1000);
