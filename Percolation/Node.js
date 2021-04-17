24974c42d49aaeb2d170fe1b239178bf4c673b11
George Gardner <Gpgardner@yahoo.com>
2021-04-17 16:16:29 -0400

    small refactor to make more readable

--- Percolation/Node.js
+++ Percolation/Node.js
@@ -1,28 +1,33 @@
 module.exports = class Node {
   row;
   col;
-  isOpen;
-  isFull;
+  open;
+  full;
   root;
-  size;
+  
 
   constructor(row, col) {
     this.row = row;
     this.col = col;
-    this.isOpen = false;
-    this.isFull = false;
+    this.open = false;
+    this.full = false;
     this.topRow = row === 0;
+    this.topRow ? (this.root = 0) : null;
   }
 
   setFull() {
-    if (this.isOpen) {
-      this.isFull = true;
+    if (this.open) {
+      this.full = true;
     } else {
       throw new Error(`This node is blocked`);
     }
   }
 
   _print() {
-    return `row: ${this.row}, col: ${this.col}, isOpen: ${this.isOpen}, topRow: ${this.topRow}`;
+    return `row: ${this.row}, col: ${this.col}`;
+  }
+
+  _printFull() {
+    return `row: ${this.row}, col: ${this.col}, isOpen: ${this.open}, topRow: ${this.topRow}, root ${this.root}, size ${this.size}`;
   }
 };
