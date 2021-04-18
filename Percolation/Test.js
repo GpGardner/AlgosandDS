e14c337f15f0bc0b718c9b0d7a4d7864b28421de
George Gardner <Gpgardner@yahoo.com>
2021-04-17 22:25:56 -0400

    test

--- Percolation/Test.js
+++ Percolation/Test.js
@@ -1,42 +1,17 @@
 const Percolation = require('./Percolation');
 const colors = require('colors/safe');
 
-const perc = new Percolation(9);
+const perc = new Percolation(4);
 let width = perc.gridSize - 1;
-
-// console.log(perc._isOpen(0, 1));
-// console.log(perc.numberOfOpenSites());
-// perc._print();
-// console.log(perc._isOpen(1, 2));
-// perc.open(1, 2);
-// perc.open(0, 2);
-// perc._print();
-// console.log(perc.ifFull2(1, 2));
 
 function randNum() {
   return Math.round(Math.random() * width);
 }
 
-// perc.open(0, 2);
-// perc.open(1, 1);
-// perc.open(2, 2);
-// perc.open(3, 2);
-// console.log(perc._isOpen(3, 2));
-// perc._print();
-// console.log(`log ${perc.isFull(3, 2)}`);
+perc.open(2, 3);
+console.log('End open 1');
 
-// console.log(`TEST: ${perc.percolates()}`);
-
-setInterval(() => {
-  if (perc.percolates()) {
-    return;
-  }
-  let value1 = randNum();
-  let value2 = randNum();
-
-  console.log(`Row: ${value1}, Column ${value2}`);
-  if (perc._findNode(value1, value2).open === false) {
-    perc.open(value1, value2);
-    perc._print();
-  }
-}, 1000);
+console.log(
+  perc._nodeAbove(1, 3) //1,3
+);
+perc._print();
